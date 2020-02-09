a0bfdd3ea216196199fbcc8a782dc50ab8e6ecbc
Ram Nad <ramnad1999@gmail.com>
2020-02-09 18:09:07 +0530

    dotenv configure

--- AbhisargaBackend/settings.py
+++ AbhisargaBackend/settings.py
@@ -14,10 +14,14 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if os.path.isfile(os.path.join(BASE_DIR, ".env")):
+    load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
+else:
+    load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env.example"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
