f8642c49ba37c876204a77708e2cc839d1461793
Ram Nad <ramnad1999@gmail.com>
2020-02-15 21:41:07 +0530

    google sign in

--- registration/urls.py
+++ registration/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import *
+
+from .views import ProfileCreateView, google_login, google_sign_in
 
 urlpatterns = [
-    path('create', ProfileCreateView.as_view(), name='signup')
+    path('create', ProfileCreateView.as_view(), name='signup'),
+    path('gsignin', google_sign_in, name="google_sign_in"),
+    path('google_login', google_login, name='google_log_in'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,10 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.views import View
+from google.auth.transport import requests as google_requests
+from google.oauth2 import id_token
 
+from AbhisargaBackend.settings import GOOGLE_CLIEND_ID
 from .forms import ProfileForm
 
 
@@ -20,3 +23,17 @@ class ProfileCreateView(View):
         except ValueError:
             return render(request, 'registration/signup.html',
                           context={'form': ProfileForm().as_p(), 'errors': form.errors})
+
+
+def google_sign_in(request):
+    return render(request, 'registration/googlelogin.html', context={'google_client_id': GOOGLE_CLIEND_ID})
+
+
+def google_login(request):
+    try:
+        token = request.POST['google-id-token']
+        user = id_token.verify_token(token, google_requests.Request(), GOOGLE_CLIEND_ID)
+        print(user)
+    except KeyError:
+        return redirect('home')
+    return redirect('home')
