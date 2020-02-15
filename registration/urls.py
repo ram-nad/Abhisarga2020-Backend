5efc82b61f9a92c83acc6e494832f2d88188fe6b
Ram Nad <ramnad1999@gmail.com>
2020-02-15 22:44:48 +0530

    google log in merged

--- registration/urls.py
+++ registration/urls.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.views import auth_logout
 from django.urls import path
 
-from .views import ProfileCreateView, google_login, UserLoginView
+from .views import ProfileCreateView, UserLoginView
 
 urlpatterns = [
     path('create', ProfileCreateView.as_view(), name='signup'),
-    path('google_login', google_login, name='google_log_in'),
     path('login', UserLoginView.as_view(), name='login'),
     path('logout', auth_logout, name='logout'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -5,7 +5,7 @@ from django.views import View
 from google.auth.transport import requests as google_requests
 from google.oauth2 import id_token
 
-from AbhisargaBackend.settings import GOOGLE_CLIEND_ID
+from AbhisargaBackend.settings import GOOGLE_CLIEND_ID, NEXT_PARAMETER
 from .forms import *
 
 
@@ -26,34 +26,43 @@ class ProfileCreateView(View):
                           context={'form': ProfileForm(request.POST).as_p(), 'errors': form.errors})
 
 
-def google_sign_in(request):
-    return render(request, 'registration/googlelogin.html', context={'google_client_id': GOOGLE_CLIEND_ID})
-
-
-def google_login(request):
-    try:
-        token = request.POST['google-id-token']
-        user = id_token.verify_token(token, google_requests.Request(), GOOGLE_CLIEND_ID)
-        print(user)
-    except KeyError:
-        return redirect('home')
-    return redirect('home')
-
-
 class UserLoginView(View):
     def get(self, request):
-        return render(request, 'registration/login.html', context={'form': LoginForm().as_p()})
+        return render(request, 'registration/login.html', context={'google_client_id': GOOGLE_CLIEND_ID})
 
     def post(self, request):
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            email, password = data['email'], data['password']
-            u = authenticate(request, email=email, password=password)
-            if u is None:
+        if 'google-id-token' in request.POST:
+            try:
+                token = request.POST['google-id-token']
+                user = id_token.verify_token(token, google_requests.Request(), GOOGLE_CLIEND_ID)
+                if user['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                    raise ValueError('Wrong issuer.')
+                u = User.objects.get(email=user['email'])
+                if u is None:
+                    return render(request, 'registration/login.html',
+                                  context={'error': 'User does not exist', 'google_client_id': GOOGLE_CLIEND_ID})
+                else:
+                    auth_login(request, u)
+                    if NEXT_PARAMETER in request.GET:
+                        return redirect(request.GET[NEXT_PARAMETER])
+                    else:
+                        return redirect('home')
+            except ValueError:
                 return render(request, 'registration/login.html',
-                              context={'form': LoginForm().as_p(), 'error': 'Wrong Email or Password'})
-            auth_login(request, u)
-            return redirect('home')
-        return render(request, 'registration/login.html',
-                      context={'form': LoginForm().as_p(), 'error': form.errors})
+                              context={'error': 'Invalid Login Attempt', 'google_client_id': GOOGLE_CLIEND_ID})
+        else:
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                email, password = data['email'], data['password']
+                u = authenticate(request, email=email, password=password)
+                if u is None:
+                    return render(request, 'registration/login.html',
+                                  context={'error': 'Wrong Email or Password', 'google_client_id': GOOGLE_CLIEND_ID})
+                auth_login(request, u)
+                if NEXT_PARAMETER in request.GET:
+                    return redirect(request.GET[NEXT_PARAMETER])
+                else:
+                    return redirect('home')
+            return render(request, 'registration/login.html',
+                          context={'error': form.errors, 'google_client_id': GOOGLE_CLIEND_ID})
