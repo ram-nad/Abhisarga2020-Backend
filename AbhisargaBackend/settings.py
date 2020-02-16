7540c1e8a49420eb635fd25f255620fb77595710
Ashutosh Chauhan <masterashu@live.in>
2020-02-16 16:54:58 +0530

    SMTP config

--- AbhisargaBackend/settings.py
+++ AbhisargaBackend/settings.py
@@ -154,3 +154,14 @@ PAYTM_INDUSTRY_TYPE_ID = os.getenv('PAYTM_INDUSTRY_TYPE_ID')
 MAILGUN_API_KEY = os.getenv('MAILGUN_API_KEY')
 MAILGUN_BASE_URL = os.getenv('MAILGUN_BASE_URL')
 MAILGUN_HOST_USER = os.getenv('MAILGUN_HOST_USER')
+
+# Email Configuration (SMTP)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = (os.getenv('EMAIL_USE_TLS') == 'True')
+# EMAIL_USE_SSL = (os.getenv('EMAIL_USE_SSL') == 'True')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
